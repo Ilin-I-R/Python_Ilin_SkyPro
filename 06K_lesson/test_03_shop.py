3ecadd4c82bc5106819379bae5236676e62ec0c9
Soliwier <soliwier.2204@gmail.com>
2026-02-15 12:24:43 +0800

    test_03_shop.py

--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -1,77 +1,82 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 
-geckodriver_path = r"F:\It\Python_Ilin_SkyPro\geckodriver.exe"
-service = Service(geckodriver_path)
-driver = webdriver.Firefox(service=service)
 
-driver.get("https://www.saucedemo.com/")
-print("Вводим логин и пароль")
-
-username_field = driver.find_element(By.ID, "user-name")
-username_field.send_keys("standard_user")
-
-password_field = driver.find_element(By.ID, "password")
-password_field.send_keys("secret_sauce")
-
-login_button = driver.find_element(By.ID, "login-button")
-login_button.click()
-
-driver.implicitly_wait(5)
-print("Добавляем товары в корзину")
-
-backpack_button = driver.find_element(
-    By.ID,
-    "add-to-cart-sauce-labs-backpack"
-)
-backpack_button.click()
-
-tshirt_button = driver.find_element(
-    By.ID,
-    "add-to-cart-sauce-labs-bolt-t-shirt"
-)
-tshirt_button.click()
+@pytest.fixture
+def setup_driver():
+    geckodriver_path = r"F:\It\Python_Ilin_SkyPro\geckodriver.exe"
+    service = Service(geckodriver_path)
+    driver = webdriver.Firefox(service=service)
+    yield driver
+    driver.quit()
 
 
-onesie_button = driver.find_element(
-    By.ID,
-    "add-to-cart-sauce-labs-onesie"
-)
-onesie_button.click()
+def test_shop_checkout(setup_driver):
+    driver = setup_driver
 
-print("Переходим в корзину")
-cart_link = driver.find_element(By.CLASS_NAME, "shopping_cart_link")
-cart_link.click()
+    driver.get("https://www.saucedemo.com/")
+    print("Вводим логин и пароль")
 
-checkout_button = driver.find_element(By.ID, "checkout")
-checkout_button.click()
+    username_field = driver.find_element(By.ID, "user-name")
+    username_field.send_keys("standard_user")
 
-print("Заполняем форму")
-first_name_field = driver.find_element(By.ID, "first-name")
-first_name_field.send_keys("Илья")
+    password_field = driver.find_element(By.ID, "password")
+    password_field.send_keys("secret_sauce")
 
-last_name_field = driver.find_element(By.ID, "last-name")
-last_name_field.send_keys("Ильин")
+    login_button = driver.find_element(By.ID, "login-button")
+    login_button.click()
 
-zip_field = driver.find_element(By.ID, "postal-code")
-zip_field.send_keys("664000")
+    driver.implicitly_wait(5)
+    print("Добавляем товары в корзину")
 
-continue_button = driver.find_element(By.ID, "continue")
-continue_button.click()
+    backpack_button = driver.find_element(
+        By.ID,
+        "add-to-cart-sauce-labs-backpack"
+    )
+    backpack_button.click()
 
-print("Сверяем итоговую сумму")
-total_element = driver.find_element(By.CLASS_NAME, "summary_total_label")
-total_text = total_element.text
-total_amount = float(total_text.split("$")[1])
-expected_total = 58.29
+    tshirt_button = driver.find_element(
+        By.ID,
+        "add-to-cart-sauce-labs-bolt-t-shirt"
+    )
+    tshirt_button.click()
 
+    onesie_button = driver.find_element(
+        By.ID,
+        "add-to-cart-sauce-labs-onesie"
+    )
+    onesie_button.click()
 
-if total_amount == expected_total:
-    print(f"Тест пройден! Итоговая сумма: ${total_amount}")
-else:
-    print(
+    print("Переходим в корзину")
+    cart_link = driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+    cart_link.click()
+
+    checkout_button = driver.find_element(By.ID, "checkout")
+    checkout_button.click()
+
+    print("Заполняем форму")
+    first_name_field = driver.find_element(By.ID, "first-name")
+    first_name_field.send_keys("Илья")
+
+    last_name_field = driver.find_element(By.ID, "last-name")
+    last_name_field.send_keys("Ильин")
+
+    zip_field = driver.find_element(By.ID, "postal-code")
+    zip_field.send_keys("664000")
+
+    continue_button = driver.find_element(By.ID, "continue")
+    continue_button.click()
+
+    print("Сверяем итоговую сумму")
+    total_element = driver.find_element(By.CLASS_NAME, "summary_total_label")
+    total_text = total_element.text
+    total_amount = float(total_text.split("$")[1])
+    expected_total = 58.29
+
+    assert total_amount == expected_total, (
         f"Ошибка! Ожидалось: ${expected_total}, но получилось: ${total_amount}"
     )
 
-driver.quit()
+    print(f"Тест пройден! Итоговая сумма: ${total_amount}")
